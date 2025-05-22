85ec6c887901552f1380083dc3d5b57d52729267
Ayush Pratap Singh <ayushdce2@gmail.com>
2025-05-22 17:26:14 +0530

    added

--- src/components/PortfolioUtility.js
+++ src/components/PortfolioUtility.js
@@ -1,5 +1,24 @@
 import underProcess from "./assets/icons/underProcess.png";
 import musiccar from "./assets/images/musiccar.PNG";
+import dashboard from "./assets/images/dashboard.PNG";
+import dashboard1 from "./assets/images/dashboard1.png";
+import LoginDesign from "./assets/images/LoginDesign.png";
+import LoginDesign1 from "./assets/images/LoginDesign1.png";
+import LoginDesign2 from "./assets/images/LoginDesign2.png";
+import calculator from "./assets/images/calculator.png";
+import todo from "./assets/images/todo.png";
+import digitalMarketing from "./assets/images/digitalMarketing.png";
+import weatherApp from "./assets/images/weatherApp.png";
+import foodApp from "./assets/images/foodApp.png";
+import dragdrop from "./assets/images/dragdrop.png";
+import notesTaking from "./assets/images/notesTaking.png";
+import digitalClock from "./assets/images/digitalClock.png";
+import countdown from "./assets/images/countdown.png";
+
+
+
+
+
 
 export const PortfolioUtility=[
     {
@@ -11,10 +30,18 @@ export const PortfolioUtility=[
 
     },
     {
-        image: underProcess,
-        title:"Admin Dashboard",
-        tags: ["React.js", "Tailwind", "HTML", "CSS","Javascript"],
-        description: "this is sample paragraph about title asdfghjkl this is sample paragraph about title asdfghjkl this is sample paragraph about title asdfghjkl"
+        image: dashboard,
+        title:" Dashboard",
+        tags: ["React.js","Responsive", "HTML", "CSS","Javascript"],
+        description: "this is sample paragraph about title asdfghjkl this is sample paragraph about title asdfghjkl this is sample paragraph about title asdfghjkl",
+        viewMore: "https://ayushdce2.github.io/dashboard1/"
+    },
+    {
+        image: dashboard1,
+        title:" Dashboard 1",
+        tags: ["HTML", "CSS","Javascript","HighChart","Sidebar"],
+        description: "this is sample paragraph about title asdfghjkl this is sample paragraph about title asdfghjkl this is sample paragraph about title asdfghjkl",
+        viewMore: "https://ayushdce1.github.io/dashboard/#"
     },
     {
         image: underProcess,
@@ -23,10 +50,11 @@ export const PortfolioUtility=[
         description: "this is sample paragraph about title asdfghjkl this is sample paragraph about title asdfghjkl this is sample paragraph about title asdfghjkl"
     },
     {
-        image: underProcess,
+        image: foodApp,
         title:"Food App",
-        tags: ["React.js", "Tailwind", "HTML", "CSS","Javascript"],
-        description: "this is sample paragraph about title asdfghjkl this is sample paragraph about title asdfghjkl this is sample paragraph about title asdfghjkl"
+        tags: ["React.js", "Tailwind", "Api", "CSS","Javascript"],
+        description: "this is sample paragraph about title asdfghjkl this is sample paragraph about title asdfghjkl this is sample paragraph about title asdfghjkl",
+        viewMore: "https://ayushdce2.github.io/food_react"
     },
     {
         image: underProcess,
@@ -45,5 +73,82 @@ export const PortfolioUtility=[
         title:"NGO Template",
         tags: ["React.js", "Tailwind", "HTML", "CSS","Javascript"],
         description: "this is sample paragraph about title asdfghjkl this is sample paragraph about title asdfghjkl this is sample paragraph about title asdfghjkl"
+    },
+    {
+        image: LoginDesign,
+        title:"Login Page ",
+        tags: ["React.js", "Responsive", "SignUp", "Forgot Password","Sign In"],
+        description: "this is sample paragraph about title asdfghjkl this is sample paragraph about title asdfghjkl this is sample paragraph about title asdfghjkl",
+        viewMore: "https://ayushdce2.github.io/hospital_mgmt/"
+    },
+    {
+        image: LoginDesign1,
+        title:"Login Page 1",
+        tags: ["React.js", "Sign In", "HTML","CSS","Javascript"],
+        description: "this is sample paragraph about title asdfghjkl this is sample paragraph about title asdfghjkl this is sample paragraph about title asdfghjkl",
+        viewMore: "https://emp-mgmt-node-6.onrender.com/#"
+    },
+    {
+        image: LoginDesign2,
+        title:"Login Page 2",
+        tags: ["React.js", "Sign In", "HTML","CSS","Javascript"],
+        description: "this is sample paragraph about title asdfghjkl this is sample paragraph about title asdfghjkl this is sample paragraph about title asdfghjkl",
+        viewMore: "https://todo-mern-front-8c90.onrender.com/"
+    },
+    {
+        image: digitalClock,
+        title:"Digital Clock App",
+        tags: ["HTML","CSS","Javascript","Responsive","-"],
+        description: "this is sample paragraph about title asdfghjkl this is sample paragraph about title asdfghjkl this is sample paragraph about title asdfghjkl",
+        viewMore: "https://ayushdce1.github.io/clock/"
+    },
+    {
+        image: countdown,
+        title:"CountDown Demo App",
+        tags: ["HTML","CSS","Javascript","Responsive","-"],
+        description: "this is sample paragraph about title asdfghjkl this is sample paragraph about title asdfghjkl this is sample paragraph about title asdfghjkl",
+        viewMore: "https://ayushdce1.github.io/countdown/"
+    },
+    {
+        image: digitalMarketing,
+        title:"Digital Marketing Agency",
+        tags: ["React.js", "HTML","CSS","Javascript","Responsive"],
+        description: "this is sample paragraph about title asdfghjkl this is sample paragraph about title asdfghjkl this is sample paragraph about title asdfghjkl",
+        viewMore: "https://ayushdce2.github.io/digitalmarketing_web/"
+    },
+    {
+        image: weatherApp,
+        title:"Weather App",
+        tags: ["React.js", "HTML","CSS","Javascript","Responsive"],
+        description: "this is sample paragraph about title asdfghjkl this is sample paragraph about title asdfghjkl this is sample paragraph about title asdfghjkl",
+        viewMore: "https://ayushdce2.github.io/weather_react/"
+    },
+    {
+        image: calculator,
+        title:"Calculator",
+        tags: ["React.js", "HTML","CSS","Javascript","Responsive"],
+        description: "this is sample paragraph about title asdfghjkl this is sample paragraph about title asdfghjkl this is sample paragraph about title asdfghjkl",
+        viewMore: "https://ayushdce2.github.io/calculator_react/"
+    },
+    {
+        image: todo,
+        title:"ToDo App",
+        tags: ["React.js", "HTML","CSS","Javascript","Responsive"],
+        description: "this is sample paragraph about title asdfghjkl this is sample paragraph about title asdfghjkl this is sample paragraph about title asdfghjkl",
+        viewMore: "https://ayushdce2.github.io/todo_react/"
+    },
+    {
+        image: dragdrop,
+        title:"Drag & Drop",
+        tags: ["HTML","CSS","Javascript","-", "-"],
+        description: "this is sample paragraph about title asdfghjkl this is sample paragraph about title asdfghjkl this is sample paragraph about title asdfghjkl",
+        viewMore: "https://ayushdce1.github.io/drag_drop/"
+    },
+    {
+        image: notesTaking,
+        title:"Notes Taking App",
+        tags: ["HTML","CSS","Javascript","-", "-"],
+        description: "this is sample paragraph about title asdfghjkl this is sample paragraph about title asdfghjkl this is sample paragraph about title asdfghjkl",
+        viewMore: "https://ayushdce1.github.io/notes/#"
     }
 ]
