becf72b11f457bbd202faa3db4cf030ceb84c6e0
Ayush Pratap Singh <ayushdce2@gmail.com>
2025-11-11 14:36:11 +0530

    add

--- src/components/PortfolioUtility.js
+++ src/components/PortfolioUtility.js
@@ -3,10 +3,13 @@ import musiccar from "./assets/images/musiccar.PNG";
 import dashboard from "./assets/images/dashboard.PNG";
 import dashboard1 from "./assets/images/dashboard1.png";
 import dashboard2 from "./assets/images/dashboard2.PNG";
+import dashboard3 from "./assets/images/dashboard3.PNG";
 import LoginDesign from "./assets/images/LoginDesign.png";
 import LoginDesign1 from "./assets/images/LoginDesign1.png";
 import LoginDesign2 from "./assets/images/LoginDesign2.png";
 import LoginDesign3 from "./assets/images/LoginDesign3.png";
+import LoginDesign4 from "./assets/images/loginDesign4.PNG";
+import doctorweb from "./assets/images/doctorweb.PNG";
 import calculator from "./assets/images/calculator.png";
 import todo from "./assets/images/todo.png";
 import digitalMarketing from "./assets/images/digitalMarketing.png";
@@ -26,6 +29,14 @@ import appointmentbookingapp from "./assets/images/Appointmentbookingapp.PNG";
 export const WebAppProjects=[
     
     {
+        image: underProcess,
+        indicator:"MERN Stack",
+        title:"Employee Management App",
+        tags: ["MERN", "Vercel", "Mongoose", "Tailwind","GitHub"],
+        description: "Streamline your team's performance with real-time insights and efficient task management, empowering your employees.",
+        viewMore: "https://emp-mgmt-mern.vercel.app/"
+    },
+    {
         image: appointmentbookingapp,
         indicator:"MERN Stack",
         title:"Appointment Booking App",
@@ -35,7 +46,7 @@ export const WebAppProjects=[
     },
     {
         image: foodApp,
-        indicator:"React , External Api Used",
+        indicator:"React , External Swiggy  Api Used",
         title:"Food App",
         tags: ["React.js", "Tailwind", "Api", "CSS","Javascript"],
         description: "A Food App enables users to browse restaurants, explore menus, and order meals for delivery or pickup",
@@ -51,48 +62,73 @@ export const WebDesignProjects=[
     {
         image: dashboard2,
         indicator:"React , Design Only",
-        title:"Patient Dashboard",
+        title:"Patient Dashboard Design",
         tags: ["React.js", "Responsive","for Patient", "tailwind","vite"],
         description: "This dashboard provides an overview of patient appointments, showing summary statistics and detailed appointment records. ",
         viewMore: "https://ayushdce2.github.io/patient_dashboard/"
     },
-    
     {
         image: dashboard,
         indicator:"React , Design Only",
-        title:" Dashboard",
+        title:" HRMS Dashboard Design",
         tags: ["React.js","Responsive", "HTML", "CSS","Javascript"],
         description: "A Dashboard provides a visual overview of key data and performance metrics in real time. It helps users make informed decisions through interactive charts, reports.",
         viewMore: "https://ayushdce2.github.io/dashboard1/"
+    },{
+        image: dashboard3,
+        indicator:"React , Design Only",
+        title:"Employee Dashboard Design",
+        tags: ["React.js", "Responsive","for Patient", "tailwind","vite"],
+        description: "This dashboard provides an overview of patient appointments, showing summary statistics and detailed appointment records. ",
+        viewMore: "https://ayushdce2.github.io/emp_dashboard/"
+    },
+    
+    
+    {
+        image: doctorweb,
+        indicator:"React , Design Only",
+        title:"Doctor Website Design",
+        tags: ["React.js", "Responsive","for Doctor", "tailwind","vite"],
+        description: "A doctor website makes healthcare more accessible by allowing patients to book appointments, access medical information, and consult doctors online anytime.",
+        viewMore: "https://ayushdce2.github.io/doctor_web/"
     },
     {
         image: digitalMarketing,
         indicator:"React , Design Only",
-        title:"Digital Marketing Agency",
+        title:"Digital Marketing Agency Design",
         tags: ["React.js", "HTML","CSS","Javascript","Responsive"],
         description: "A Digital Marketing Agency helps businesses grow online through targeted strategies like SEO, social media marketing, and paid advertising",
         viewMore: "https://ayushdce2.github.io/digitalmarketing_web/"
     },
     {
-        image: LoginDesign,
-        indicator:"React , Design Only",
-        title:"Login Page ",
-        tags: ["React.js", "Responsive", "SignUp", "Forgot Password","Sign In"],
-        description: "A Login Page allows users to securely access their accounts by entering valid credentials. ",
-        viewMore: "https://ayushdce2.github.io/hospital_mgmt/"
-    },
-    {
         image: LoginDesign1,
         indicator:"React , Design Only",
-        title:"Login Page 1",
+        title:"Login Page 1 Design",
         tags: ["React.js", "Sign In", "HTML","CSS","Javascript"],
         description: "A Login Page allows users to securely access their accounts by entering valid credentials. ",
         viewMore: "https://emp-mgmt-node-6.onrender.com/#"
     },
     {
+        image: LoginDesign4,
+        indicator:"React , Design Only",
+        title:"Login Page 2 Design ",
+        tags: ["React.js", "Responsive", "SignUp", "Forgot Password","Sign In"],
+        description: "A Login Page allows users to securely access their accounts by entering valid credentials. ",
+        viewMore: "https://ayushdce2.github.io/login2/"
+    },
+    {
+        image: LoginDesign,
+        indicator:"React , Design Only",
+        title:"Login Page 3 Design ",
+        tags: ["React.js", "Responsive", "SignUp", "Forgot Password","Sign In"],
+        description: "A Login Page allows users to securely access their accounts by entering valid credentials. ",
+        viewMore: "https://ayushdce2.github.io/login3/"
+    },
+    
+    {
         image: LoginDesign2,
         indicator:"React , Design Only",
-        title:"Login Page 2",
+        title:"Login Page 4 Design",
         tags: ["React.js", "Sign In", "HTML","CSS","Javascript"],
         description: "A Login Page allows users to securely access their accounts by entering valid credentials. ",
         viewMore: "https://todo-mern-front-8c90.onrender.com/"
@@ -100,16 +136,16 @@ export const WebDesignProjects=[
     {
         image: LoginDesign3,
         indicator:"React , Design Only",
-        title:"Login Page 3",
+        title:"Login Page 5 Design",
         tags: ["React.js", "Sign In", "HTML","CSS","Javascript"],
         description: "A Login Page allows users to securely access their accounts by entering valid credentials. ",
-        viewMore: "https://ayushdce2.github.io/ecommerce_mern/"
+        viewMore: "https://ayushdce2.github.io/login1/"
     },
     
     {
         image: dashboard1,
         indicator:"JavaScript",
-        title:" Dashboard 1",
+        title:" Dashboard 1 Design",
         tags: ["HTML", "CSS","Javascript","HighChart","Sidebar"],
         description: "A Dashboard provides a visual overview of key data and performance metrics in real time. It helps users make informed decisions through interactive charts, reports.",
         viewMore: "https://ayushdce1.github.io/dashboard/#"
@@ -118,7 +154,7 @@ export const WebDesignProjects=[
     {
         image: musiccar,
         indicator:"Wordpress",
-        title:"MusicCar",
+        title:"MusicCar Design",
         tags: ["Wordpress", "Elementor", "HTML", "CSS","WooCommerce"],
         description: "Developed a responsive WordPress website using Elementor with custom-designed pages tailored for user engagement. ",
         viewMore: "https://drive.google.com/file/d/131J9NJ3SZiv6kkymRAV4HLfTR0XgOmPQ/view?usp=sharing"
